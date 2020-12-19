67dcb0174eccee4b227cc2d094d0aebb442ba43b
Sachin Acharya <Idoncare@sachinhck.com>
2020-12-19 20:22:33 +0545

    CHANGED WEB API

--- modules/weatherForcaster.py
+++ modules/weatherForcaster.py
@@ -1,10 +1,12 @@
 import wolframalpha # pip install wolframalpha api
-app = wolframalpha.Client("PK86X8-P724WU8UQG")
+import os
+
+client_api = os.environ.get('WHEATHER_API')
+
+app = wolframalpha.Client(client_api)
 
 def AskForDetails(query):
     res = app.query(query)
     output = next(res.results).text
-    # print(output)
     return output
 
-# AskForDetails("Nepal")
