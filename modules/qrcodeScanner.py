e6a9872fffcda0168c6df76a08ba6ce697029c34
OWNER <acharyaraj71@gmail.com>
2021-01-17 23:50:21 +0545

    Little be twitched and little bit twist with a small Upgrade in Processing time

--- modules/qrcodeScanner.py
+++ modules/qrcodeScanner.py
@@ -1,21 +1,22 @@
-import cv2
-import pyzbar.pyzbar as pyzbar
-import pyautogui
+# import cv2
+# import pyautogui
+# from pyzbar import decode
 
-img = cv2.VideoCapture(0)
-font = cv2.FONT_HERSHEY_PLAIN
+# img = cv2.VideoCapture(0)
+# font = cv2.FONT_HERSHEY_PLAIN
 
 def MainFunc():
-    while True:
-        if pyautogui.keyDown('c'):
-            return False
-        _, frame = img.read()
-        # cv2.addText("Sachin Accharya")
-        decodedObjects = pyzbar.decode(frame)
-        for obj in decodedObjects:
-            output =f'''Output: {str(obj.data).split("'")[1]}'''
-            cv2.putText(frame, output, (50, 50), font, 1, (255, 0, 0), 1)
-        cv2.imshow("Output Video", frame)
-        key = cv2.waitKey(1)
-        if key == 27:
-            break
+#     while True:
+#         if pyautogui.keyDown('c'):
+#             return False
+#         _, frame = img.read()
+#         # cv2.addText("Sachin Accharya")
+#         decodedObjects = decode(frame)
+#         for obj in decodedObjects:
+#             output =f'''Output: {str(obj.data).split("'")[1]}'''
+#             cv2.putText(frame, output, (50, 50), font, 1, (255, 0, 0), 1)
+#         cv2.imshow("Output Video", frame)
+#         key = cv2.waitKey(1)
+#         if key == 27:
+#             break
+    print("Hello World")
